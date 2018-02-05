43d67806635a7161c2373997ae086e57d60ec9f4
Rocío Fernandez <rocio@besepa.com>
2018-02-05 10:57:56 +0100

    module show me the money

--- show_me_the_money.rb
+++ show_me_the_money.rb
@@ -1,10 +1,9 @@
 require 'awesome_print'
 load "bank_api_client.rb"
+#load "transfer_agent.rb"
 
-class ShowMeTheMoney
-
-	def initialize
-	end
+module ShowMeTheMoney
+	extend self
 
 	def run
 		jim_token, jim_bank, jim_bank_account = create_jim_with_bank_account
@@ -17,9 +16,11 @@ class ShowMeTheMoney
 	def create_jim_with_bank_account
 		jim_token = BankApiClient.new.create_user("Jim", "jim@gmail.com", "jim_password" )
 		jim_bank  = BankApiClient.new(jim_token).create_bank("jim bank")
+		ap jim_bank
 		jim_bank_id = jim_bank["id"]
 		jim_bank_account = BankApiClient.new(jim_token).
 											create_bank_account( jim_bank_id, "ES12345", 2050000)
+		ap jim_bank_account
 		jim_bank_account_id = jim_bank_account["id"]
 		return jim_token, jim_bank_id, jim_bank_account_id
 	end
@@ -27,17 +28,19 @@ class ShowMeTheMoney
  	def create_emma_with_bank_accont
 		emma_token = BankApiClient.new.create_user("Emma", "emma@gmail.com", "jim_password" )
 		emma_bank  = BankApiClient.new(emma_token).create_bank("emma bank")
+		ap emma_bank
 		emma_bank_id = emma_bank["id"]
 		emma_bank_account = BankApiClient.new(emma_token).
 											create_bank_account( emma_bank_id, "ES6789", 1)
+		ap emma_bank_account
 		emma_bank_account_id = emma_bank_account["id"]
 		return emma_bank_account_id
  	end
 
- 	def make_transfer(user_token, bank, origin_id, destination_id)
- 		BankApiClient.new(user_token).create_payment( bank, origin_id,  destination_id, 2000000)
+ 	def self.make_transfer(user_token, bank, origin_id, destination_id)
+ 		TransferAgent.new(user_token).calculate_payment( bank, origin_id,  destination_id, 2000000)
  	end
 
 end
 
-ShowMeTheMoney.new.run
+ShowMeTheMoney.run
